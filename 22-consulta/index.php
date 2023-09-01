4067ca1cfccc9e75cf90bc7d7228d198ba96affa
Manuel <manuel.zerg@gmail.com>
2023-09-01 13:17:39 -0600

    22-consulta

---
+++ 22-consulta/index.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Problema</title>
+    </head>
+    <body>
+        <form action="pagina2.php" method="post">
+            Ingrese el nombre del alumno a consultar:
+            <input type="text" name="nombre">
+            <br>
+            <input type="submit" value="buscar">
+        </form>
+    </body>
+</html>
