d668b3d586e4a8c08875941e0635e023e4e0ff76
Manuel <manuel.zerg@gmail.com>
2023-09-21 14:31:09 -0600

    27-listado and 28-funcion count de sql

---
+++ 27-listado/index.php
@@ -0,0 +1,14 @@
+<html>
+    <head>
+        <title>Problema</title>
+    </head>
+    <body>
+        <form action="pagina2.php" mothod="post">
+            Codigo del alumno:
+            <input type="text" name="codigo" id="" /> 
+            <br />
+            <input type="submit" value="confirmar">
+        </form>
+    </body>
+
+</html>
